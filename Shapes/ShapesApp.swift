2bab864305eb37710f761d99e87e110757e09fdc
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-23 12:39:38 +0600

    project init

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Shah Md Imran Hossain on 23/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
