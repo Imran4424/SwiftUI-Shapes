8f7034ebf51b1046965e9b09ba28e8ef4deb9e75
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-26 02:09:30 +0600

    imagePaint for border

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -71,7 +71,7 @@ struct ContentView: View {
     var body: some View {
         Text("Imran Hossain")
             .frame(width: 300, height: 300)
-            .background(Image("singapore"))
+            .border(ImagePaint(image: Image("singapore")), width: 30)
     }
 }
 
