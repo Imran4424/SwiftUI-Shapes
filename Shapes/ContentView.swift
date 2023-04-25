e334f9eaf035cac64d16746262fa23ba860c272b
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-25 11:02:27 +0600

    fix Value of type 'Arc' has no member 'strokeBorder' with InsettableShape

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -20,10 +20,11 @@ struct Triangle: Shape {
     }
 }
 
-struct Arc: Shape {
+struct Arc: InsettableShape {
     let startAngle: Angle
     let endAngle: Angle
     let clockWise: Bool
+    var insetAmount = 0.0
     
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
@@ -31,12 +32,17 @@ struct Arc: Shape {
         let modifiedEnd = endAngle - rotationAdjustment
         var path = Path()
         
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
         
         return path
     }
     
-    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        
+        return arc
+    }
 }
 
 struct ContentView: View {
