3233eaa544229ada1a7a2b61c93fe8a461aba66e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-26 12:25:22 +0600

    Apply blur and saturation

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -108,25 +108,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            ZStack {
-                Circle()
-                    .fill(.red)
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.green)
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
+            Image("singapore")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
             
             Slider(value: $amount)
                 .padding()
