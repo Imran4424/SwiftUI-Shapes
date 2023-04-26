2878b5824541a9b31d641e6f7be226bac4452fab
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-26 06:33:39 +0600

    screen blend mode

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -104,9 +104,36 @@ struct ColorCyclingCircle: View {
 }
 
 struct ContentView: View {
+    @State private var amount = 0.0
+    
     var body: some View {
-        Image("singapore")
-            .colorMultiply(.red)
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
